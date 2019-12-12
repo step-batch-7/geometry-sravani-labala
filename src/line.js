42807e0290b08c14f4339c4f5b88e362dc76ddff
Sravani Labala <sravani.labala@gmail.com>
2019-12-12 15:59:24 +0530

    added isEqualTo method

--- src/line.js
+++ src/line.js
@@ -1,6 +1,20 @@
 class Line {
+  constructor(x1, y1, x2, y2) {
+    this.x1 = x1;
+    this.y1 = y1;
+    this.x2 = x2;
+    this.y2 = y2;
+  }
   toString() {
-    return "something";
+    return "working";
+  }
+  isEqualTo(x1, y1, x2, y2) {
+    let line = new Line(x1, x2, y1, y2);
+    line = line.lineSegment();
+    return line == (x1, y1, x2, y2);
+  }
+  lineSegment() {
+    return this.x1, this.y1, this.x2, this.y2;
   }
 }
 
--- test/testLine.js
+++ test/testLine.js
@@ -2,10 +2,15 @@ const assert = require("assert");
 const { Line } = require("./../src/line.js");
 
 describe("line", function() {
+  let line = new Line(1, 2, 3, 4);
   describe("toString", function() {
-    it("should give something", function() {
-      let line = new Line();
-      assert.strictEqual(line.toString(), "something");
+    it("should give 'something' to indicate the class is working", function() {
+      assert.strictEqual(line.toString(), "working");
+    });
+  });
+  describe("isEqualTo", function() {
+    it("should give true if the line parameters and required parameters are equal", function() {
+      assert.strictEqual(line.isEqualTo(1, 2, 3, 4), true);
     });
   });
 });
