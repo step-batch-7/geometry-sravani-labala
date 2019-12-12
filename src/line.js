2849d76558e6dc530a332119163f6941d95bf1a6
Sravani Labala <sravani.labala@gmail.com>
2019-12-12 16:21:30 +0530

    time up

--- src/line.js
+++ src/line.js
@@ -9,12 +9,8 @@ class Line {
     return "working";
   }
   isEqualTo(x1, y1, x2, y2) {
-    let line = new Line(x1, x2, y1, y2);
-    line = line.lineSegment();
-    return line == (x1, y1, x2, y2);
-  }
-  lineSegment() {
-    return this.x1, this.y1, this.x2, this.y2;
+    //  let line = new Line(x1, y1, x2, y2);
+    return new Line(x1, y1, x2, y2);
   }
 }
 
--- test/testLine.js
+++ test/testLine.js
@@ -2,14 +2,16 @@ const assert = require("assert");
 const { Line } = require("./../src/line.js");
 
 describe("line", function() {
-  let line = new Line(1, 2, 3, 4);
   describe("toString", function() {
-    it("should give 'something' to indicate the class is working", function() {
+    it("should give 'working' to indicate the class is working", function() {
+      let line = new Line();
       assert.strictEqual(line.toString(), "working");
     });
   });
+
   describe("isEqualTo", function() {
     it("should give true if the line parameters and required parameters are equal", function() {
+      let line = new Line(1, 2, 3, 4);
       assert.strictEqual(line.isEqualTo(1, 2, 3, 4), true);
     });
   });
