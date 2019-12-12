f19c3442ae67f2ca8f6798a6d9d3d8f3d31122f9
Sravani Labala <sravani.labala@gmail.com>
2019-12-12 19:44:54 +0530

    changed toString representation

--- src/line.js
+++ src/line.js
@@ -1,16 +1,11 @@
 class Line {
   constructor(x1, y1, x2, y2) {
-    this.x1 = x1;
-    this.y1 = y1;
-    this.x2 = x2;
-    this.y2 = y2;
+    this.endA = { x: x1, y: y1 };
+    this.endB = { x: x2, y: y2 };
   }
   toString() {
-    return "working";
-  }
-  isEqualTo(value1, value2) {
-    return value1 == value2;
+    return `Line (${this.endA.x},${this.endA.y}) (${this.endB.x},${this.endB.y})`;
   }
 }
 
-module.exports = { Line };
+module.exports = Line;
--- test/testLine.js
+++ test/testLine.js
@@ -1,21 +1,11 @@
 const assert = require("assert");
-const { Line } = require("./../src/line.js");
+const Line = require("./../src/line.js");
 
 describe("line", function() {
   describe("toString", function() {
     it("should give 'working' to indicate the class is working", function() {
-      let line = new Line();
-      assert.strictEqual(line.toString(), "working");
-    });
-  });
-
-  describe("isEqualTo", function() {
-    it("should give true if the line parameters and required parameters are equal", function() {
       let line = new Line(1, 2, 3, 4);
-      assert.strictEqual(line.isEqualTo(1, line.x1), true);
-      assert.strictEqual(line.isEqualTo(2, line.y1), true);
-      assert.strictEqual(line.isEqualTo(3, line.x2), true);
-      assert.strictEqual(line.isEqualTo(4, line.y2), true);
+      assert.strictEqual(line.toString(), "Line (1,2) (3,4)");
     });
   });
 });
