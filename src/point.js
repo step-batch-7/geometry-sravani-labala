13dd617584cfc7a4528deb467ea9c1596600a8b7
Sravani Labala <sravani.labala@gmail.com>
2019-12-14 11:07:14 +0530

    added isEqualTo method in point class

--- src/point.js
+++ src/point.js
@@ -3,11 +3,18 @@ class Point {
     this.x = abscissa;
     this.y = ordinate;
   }
+
   toString() {
     return `[Point @(${this.x},${this.y})]`;
   }
+
   visit(functionRef) {
     return functionRef(this.x, this.y);
+  }
+
+  isEqualTo(other) {
+    if (!(other instanceof Point)) return false;
+    return this.x === other.x && this.y === other.y;
   }
 }
 
--- test/testPoint.js
+++ test/testPoint.js
@@ -9,19 +9,36 @@ describe("class", function() {
     });
   });
   describe("visit", function() {
-    it("should give the sum of the coordinates", function() {
+    it("should give the result of the operation", function() {
       const point = new Point(2, 3);
       assert.strictEqual(
         point.visit((x, y) => x + y),
         5
       );
-    });
-    it("should give the product of the coordinates", function() {
-      const point = new Point(2, 3);
       assert.strictEqual(
         point.visit((x, y) => x * y),
         6
       );
+    });
+  });
+
+  describe("isEqualTo", function() {
+    it("should give true if both points are similar", function() {
+      const point = new Point(2, 3);
+      const other = new Point(2, 3);
+      assert.isTrue(point.isEqualTo(other));
+    });
+
+    it("should give false if both points are not similar", function() {
+      const point = new Point(2, 3);
+      const other = new Point(1, 3);
+      assert.isFalse(point.isEqualTo(other));
+    });
+
+    it("should give false if the type of 'other' doesn't belongs to the class Point", function() {
+      const point = new Point(2, 3);
+      const other = { x: 2, y: 3 };
+      assert.isFalse(point.isEqualTo(other));
     });
   });
 });
